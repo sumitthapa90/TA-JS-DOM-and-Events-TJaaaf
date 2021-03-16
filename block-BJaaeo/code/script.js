5eeacc48dad343ecca3078ee6c261ddd39caa24b
sumit <sumitthapa90@gmail.com>
2021-03-16 13:33:36 +0530

    complete clock assingment again

--- block-BJaaeo/code/script.js
+++ block-BJaaeo/code/script.js
@@ -2,9 +2,6 @@ let input = document.getElementById("input");
 console.log(input);
 function getInput(i) {
   var x = i;
-
-  console.log(x);
-
   if (x === "=") {
     input.value = eval(input.value);
   } else if (x === "C") {

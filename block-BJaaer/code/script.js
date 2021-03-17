1f93460ef1206528ea6efe2020f578d8860eba02
sumit <sumitthapa90@gmail.com>
2021-03-18 00:12:09 +0530

    complete

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,104 +1,83 @@
-// let form = document.querySelector(".form");
-// let model = document.querySelector(".model");
-// let button = document.querySelector("btn");
-// let inputName = document.querySelector(".input-name");
-// let inputEmail = document.querySelector(".input-email");
-// let inputChoice = document.querySelector(".input-choise");
-// let inputColor = document.querySelector(".input-color");
-// let inputRange = document.querySelector(".input-range");
-// let inputDrone = document.querySelector(".input-drone");
-// let inputTerm = document.querySelector("input-terms");
-
-// let userInfo = {};
-
-// function handleInput(event) {
-//   event.preventDefault();
-
-//   userInfo.name = event.target.text.value;
-//   inputName.innerText = userInfo.name;
-
-//   userInfo.email = event.target.text.value;
-//   inputEmail.innerText = userInfo.email;
-
-//   userInfo.choice = event.target.text.value;
-//   inputChoice.innerText = userInfo.choice;
-
-//   userInfo.coloe = event.target.text.value;
-//   inputColor.innerText = userInfo.color;
-
-//   userInfo.range = event.target.text.value;
-//   inputRange.innerText = userInfo.name;
-
-//   userInfo.range = event.target.text.value;
-//   inputDrone.innerText = userInfo.drone;
-
-//   userInfo.terms = event.target.text.value;
-//   if (userInfo.terms === "on") {
-//     inputTerm.innerText = "You Gree to terms and condition";
-//   } else {
-//     inputTerm.innerText = "";
-//   }
-
-//   form.style.display = "none";
-//   model.style.display = "flex";
-// }
-// console.log(userInfo);
-
-// form.addEventListener("submit", handleInput);
-// btn.addEventListener("click", () => {
-//   modal.style.display = "none";
-//   form.style.display = "block";
-// });
-
 let form = document.querySelector("form");
-let modal = document.querySelector(".modal");
-let cancelBtn = document.querySelector(".cancel-btn");
-let displayName = document.querySelector(".display-name");
-let displayEmail = document.querySelector(".display-email");
-let displayChoice = document.querySelector(".display-choice");
-let displayColor = document.querySelector(".display-color");
-let displayRange = document.querySelector(".display-range");
-let displayDrone = document.querySelector(".display-drone");
-let displayTerms = document.querySelector(".display-terms");
+let main = document.querySelector(".main");
+let modelLay = document.querySelector(".overlay");
 
-let userInfo = {};
-
-function handleInput(event) {
-  event.preventDefault();
-
-  userInfo.name = event.target.text.value;
-  displayName.innerText = userInfo.name;
-
-  userInfo.email = event.target.email.value;
-  displayEmail.innerText = userInfo.email;
-
-  userInfo.choice = event.target.choice.value;
-  displayChoice.innerText = userInfo.choice;
-
-  userInfo.color = event.target.color.value;
-  displayColor.innerText = userInfo.color;
-
-  userInfo.range = event.target.range.value;
-  displayRange.innerText = userInfo.range;
-
-  userInfo.drone = event.target.drone.value;
-  displayDrone.innerText = userInfo.drone;
-
-  userInfo.terms = event.target.terms.value;
-  if (userInfo.terms === "on") {
-    displayTerms.innerText = "You agree to Terms and Conditions";
-  } else {
-    displayTerms.innerText = "";
-  }
-
-  form.style.display = "none";
-  modal.style.display = "flex";
+{
+  /* <div class="container">
+        <a href="#" class="click">Click</a>
+        <div>
+          <h2>Hello Sameer</h2>
+          <p>Email: <span>isameer@gamil.com</span></p>
+          <p>You Love: <span>Movies</span></p>
+          <p>color: <span>#ffffff</span></p>
+          <p>Rating: <span>8</span></p>
+          <p>Book Genre: Fiction <span>#fffff</span></p>
+          <p>You agree to Terms and Condition</p>
+        </div>
+      </div> */
 }
-console.log(userInfo);
+let obj = {};
 
-form.addEventListener("submit", handleInput);
+function createContainer(obj) {
+  modelLay.innerHTML = "";
+  let div = document.createElement("div");
+  div.classList.add("container");
+  let a = document.createElement("a");
+  a.setAttribute("href", "#");
+  a.classList.add("click");
+  a.innerText = `Click`;
+  let div2 = document.createElement("div");
+  let h2 = document.createElement("h2");
+  h2.innerText = `${obj.name}`;
+  let emailP = document.createElement("p");
+  emailP.innerText = `Email: ${obj.email}`;
+  let moviesP = document.createElement("p");
+  moviesP.innerText = `Yor Lov: ${obj.gender}`;
+  let colorP = document.createElement("p");
+  colorP.innerText = `color: ${obj.color}`;
+  let ratingP = document.createElement("p");
+  ratingP.innerText = `Rating: ${obj.rating}`;
+  let bookGenreP = document.createElement("p");
+  bookGenreP.innerText = `${obj.fiction} ${obj.color}`;
+  let agreeA = document.createElement("a");
+  agreeA.innerText = `${
+    obj.terms === true
+      ? "You agree to Terms and Condition"
+      : "You do not agree to terms and condition "
+  }`;
+  div2.append(h2, emailP, moviesP, colorP, ratingP, bookGenreP, agreeA);
+  div.append(a, div2);
+  modelLay.append(div);
+}
 
-cancelBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-  form.style.display = "block";
-});
+function heandelerFunction(event){
+  event.preventDefault()
+  let name = form.text.value;
+  let email = form.email.value;
+  let gender = form.gender.value;
+  let color = form.color.value;
+  let rating = form.range.value;
+  let fiction  = form.drone.value;
+  let check = form.terms.checked;
+
+  
+  console.log(check)
+  obj.name  = name;
+  obj.email = email;
+  obj.gender = gender;
+  obj.color = color;
+  obj.rating = rating;
+  obj.fiction = fiction;
+  obj.terms = check;
+  obj["data-id"] = new Date();
+  createContainer(obj);
+  
+  modelLay.classList.remove("close");
+  let btn = document.querySelector(".click");
+  btn.addEventListener("click" , ()=>{
+  modelLay.classList.add("close");
+})
+}
+
+
+form.addEventListener('submit' ,heandelerFunction)
